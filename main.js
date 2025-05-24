c4ad069c75f2e3fbc3ca191911df3535c2d4f3b0
auroruse <159697917+auroruse@users.noreply.github.com>
2025-05-24 10:39:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //set values according to the URL parameters if it exists, using defaults if they don't
 const pageUrl = new URLSearchParams(location.search);
-let daysPerYear = pageUrl.get("daysperyear") ? pageUrl.get("daysperyear")/1 : 6;
-let lastDateChange = pageUrl.get("lastdatechange") ? pageUrl.get("lastdatechange")/1 : 1740780000000; //JS time adds three zeroes to UNIX time
-let lastDateEpoch = pageUrl.get("lastdateepoch") ? pageUrl.get("lastdateepoch")/1 : -17514150720000;
+let daysPerYear = pageUrl.get("daysperyear") ? pageUrl.get("daysperyear")/1 : 73;
+let lastDateChange = pageUrl.get("lastdatechange") ? pageUrl.get("lastdatechange")/1 : 1747087200000; //JS time adds three zeroes to UNIX time
+let lastDateEpoch = pageUrl.get("lastdateepoch") ? pageUrl.get("lastdateepoch")/1 : -1577930400000;
 let fixedYears = pageUrl.get("fixedyears") ? pageUrl.get("fixedyears") === "true" : false;
 
 //change the HTML elements to reflect the values
